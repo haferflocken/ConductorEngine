0852cce75b47f5b82c604a59bb64399de46c3523
haferflocken <mooilo.ueu@gmail.com>
2018-05-26 17:59:48 -0700

    First thoughts on getting the client to mirror data from the server

--- IslandGame/islandgame/client/IslandGameClient.h
+++ IslandGame/islandgame/client/IslandGameClient.h
@@ -1,15 +1,19 @@
 #pragma once
 
+#include <behave/ActorManager.h>
 #include <client/IClient.h>
+
+namespace IslandGame { class IslandGameData; }
 
 namespace IslandGame::Client
 {
 class IslandGameClient : public ::Client::IClient
 {
+	const IslandGameData& m_gameData;
+	Behave::ActorManager m_actorManager;
+
 public:
-	IslandGameClient(::Client::ConnectedHost& connectedHost)
-		: IClient(connectedHost)
-	{}
+	IslandGameClient(const IslandGameData& gameData, ::Client::ConnectedHost& connectedHost);
 
 	void Update() override;
 };
--- IslandGame/src/islandgame/client/IslandGameClient.cpp
+++ IslandGame/src/islandgame/client/IslandGameClient.cpp
@@ -1,8 +1,20 @@
 #include <islandgame/client/IslandGameClient.h>
 
+#include <islandgame/IslandGameData.h>
+
+#include <behave/BehaveContext.h>
 #include <client/ConnectedHost.h>
+
+IslandGame::Client::IslandGameClient::IslandGameClient(
+	const IslandGameData& gameData, ::Client::ConnectedHost& connectedHost)
+	: IClient(connectedHost)
+	, m_gameData(gameData)
+	, m_actorManager(gameData.GetActorComponentFactory())
+{}
 
 void IslandGame::Client::IslandGameClient::Update()
 {
-	
+	// TODO somehow stuff gets mirrored from the host??
+	const Behave::BehaveContext context{ m_gameData.GetBehaviourTreeManager() };
+	m_actorManager.Update(context);
 }
