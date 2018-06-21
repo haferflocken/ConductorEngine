9f5e60e2b9edd95a62b1c272ac2d78977ee8318c
haferflocken <mooilo.ueu@gmail.com>
2018-06-20 22:11:05 -0700

    Load schema field parameters from JSON.

--- Amp/src/assets/InfoSchema.cpp
+++ Amp/src/assets/InfoSchema.cpp
@@ -14,6 +14,13 @@ const Util::StringHash k_fieldsHash = Util::CalcHash("fields");
 const Util::StringHash k_fieldTypeHash = Util::CalcHash("fieldType");
 const Util::StringHash k_fieldIDHash = Util::CalcHash("fieldID");
 
+const Util::StringHash k_fieldDefaultValueHash = Util::CalcHash("defaultValue");
+const Util::StringHash k_fieldMinValueHash = Util::CalcHash("minValue");
+const Util::StringHash k_fieldMaxValueHash = Util::CalcHash("maxValue");
+
+const Util::StringHash k_fieldAcceptedTypesHash = Util::CalcHash("acceptedTypes");
+const Util::StringHash k_fieldMemberIDsHash = Util::CalcHash("memberIDs");
+
 InfoSchemaFieldType ParseType(const char* const typeString)
 {
 	if (strcmp("bool", typeString) == 0)
@@ -74,7 +81,7 @@ InfoSchema InfoSchema::MakeFromJSON(const JSON::JSONObject& jsonObject)
 		const JSON::JSONNumber* const candidateID = field.FindNumber(k_fieldIDHash);
 		if (candidateID == nullptr)
 		{
-			Dev::LogWarning("Skipped a field which was missing an ID in the field array.");
+			Dev::LogWarning("Skipped a field which was missing an ID.");
 			continue;
 		}
 		const uint16_t fieldID = static_cast<uint16_t>(candidateID->m_number);
@@ -82,15 +89,14 @@ InfoSchema InfoSchema::MakeFromJSON(const JSON::JSONObject& jsonObject)
 		const JSON::JSONString* const candidateType = field.FindString(k_fieldTypeHash);
 		if (candidateType == nullptr)
 		{
-			Dev::LogWarning("Skipped field [%u] with a missing type in the field array.", fieldID);
+			Dev::LogWarning("Skipped field [%u] with a missing type.", fieldID);
 			continue;
 		}
 
 		const InfoSchemaFieldType fieldType = ParseType(candidateType->m_string.c_str());
 		if (fieldType == InfoSchemaFieldType::Invalid)
 		{
-			Dev::LogWarning("Skipped field [%u] with unknown type [%s] in the field array.", fieldID,
-				candidateType->m_string.c_str());
+			Dev::LogWarning("Skipped field [%u] with unknown type [%s].", fieldID, candidateType->m_string.c_str());
 			continue;
 		}
 
@@ -99,30 +105,105 @@ InfoSchema InfoSchema::MakeFromJSON(const JSON::JSONObject& jsonObject)
 		case InfoSchemaFieldType::Boolean:
 		{
 			InfoSchemaField booleanField = InfoSchemaField::MakeBooleanField(fieldID);
+
+			const JSON::JSONBoolean* const candidateDefault = field.FindBoolean(k_fieldDefaultValueHash);
+			if (candidateDefault != nullptr)
+			{
+				booleanField.m_booleanData.m_defaultValue = candidateDefault->m_boolean;
+			}
+
 			outSchema.m_fields.Add(std::move(booleanField));
 			break;
 		}
 		case InfoSchemaFieldType::Float:
 		{
 			InfoSchemaField floatField = InfoSchemaField::MakeFloatField(fieldID);
+
+			const JSON::JSONNumber* const candidateDefault = field.FindNumber(k_fieldDefaultValueHash);
+			const JSON::JSONNumber* const candidateMin = field.FindNumber(k_fieldMinValueHash);
+			const JSON::JSONNumber* const candidateMax = field.FindNumber(k_fieldMaxValueHash);
+			if (candidateDefault != nullptr)
+			{
+				floatField.m_floatData.m_defaultValue = static_cast<float>(candidateDefault->m_number);
+			}
+			if (candidateMin != nullptr)
+			{
+				floatField.m_floatData.m_minimumValue = static_cast<float>(candidateMin->m_number);
+			}
+			if (candidateMax != nullptr)
+			{
+				floatField.m_floatData.m_maximumValue = static_cast<float>(candidateMax->m_number);
+			}
+
 			outSchema.m_fields.Add(std::move(floatField));
 			break;
 		}
 		case InfoSchemaFieldType::Integer:
 		{
 			InfoSchemaField integerField = InfoSchemaField::MakeIntegerField(fieldID);
+
+			const JSON::JSONNumber* const candidateDefault = field.FindNumber(k_fieldDefaultValueHash);
+			const JSON::JSONNumber* const candidateMin = field.FindNumber(k_fieldMinValueHash);
+			const JSON::JSONNumber* const candidateMax = field.FindNumber(k_fieldMaxValueHash);
+			if (candidateDefault != nullptr)
+			{
+				integerField.m_integerData.m_defaultValue = static_cast<int32_t>(candidateDefault->m_number);
+			}
+			if (candidateMin != nullptr)
+			{
+				integerField.m_integerData.m_minimumValue = static_cast<int32_t>(candidateMin->m_number);
+			}
+			if (candidateMax != nullptr)
+			{
+				integerField.m_integerData.m_maximumValue = static_cast<int32_t>(candidateMax->m_number);
+			}
+
 			outSchema.m_fields.Add(std::move(integerField));
 			break;
 		}
 		case InfoSchemaFieldType::InstanceReference:
 		{
 			InfoSchemaField instanceReferenceField = InfoSchemaField::MakeInstanceReferenceField(fieldID);
+
+			const JSON::JSONArray* const candidateAcceptedTypes = field.FindArray(k_fieldAcceptedTypesHash);
+			if (candidateAcceptedTypes != nullptr)
+			{
+				for (const auto& candidateAcceptedType : *candidateAcceptedTypes)
+				{
+					if (candidateAcceptedType->GetType() != JSON::ValueType::String)
+					{
+						Dev::LogWarning("Skipped non-string accepted type in field [%u].", fieldID);
+						continue;
+					}
+					const JSON::JSONString& acceptedType =
+						*dynamic_cast<const JSON::JSONString*>(candidateAcceptedType.Get());
+					instanceReferenceField.m_instanceReferenceData.m_acceptedTypes.Add(acceptedType.m_string);
+				}
+			}
+
 			outSchema.m_fields.Add(std::move(instanceReferenceField));
 			break;
 		}
 		case InfoSchemaFieldType::Group:
 		{
 			InfoSchemaField groupField = InfoSchemaField::MakeGroupField(fieldID);
+
+			const JSON::JSONArray* const candidateMemberFieldIDs = field.FindArray(k_fieldMemberIDsHash);
+			if (candidateMemberFieldIDs != nullptr)
+			{
+				for (const auto& candidateMemberFieldID : *candidateMemberFieldIDs)
+				{
+					if (candidateMemberFieldID->GetType() != JSON::ValueType::Number)
+					{
+						Dev::LogWarning("Skipped non-number member field ID in field [%u].", fieldID);
+						continue;
+					}
+					const JSON::JSONNumber& memberFieldID =
+						*dynamic_cast<const JSON::JSONNumber*>(candidateMemberFieldID.Get());
+					groupField.m_groupData.m_memberFieldIDs.Add(static_cast<uint16_t>(memberFieldID.m_number));
+				}
+			}
+
 			outSchema.m_fields.Add(std::move(groupField));
 			break;
 		}
