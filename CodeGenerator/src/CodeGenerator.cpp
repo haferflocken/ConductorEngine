9f8de12b7641a5d396bdf234a9e2266f87a4c81c
haferflocken <mooilo.ueu@gmail.com>
2018-06-20 22:31:41 -0700

    Added a CodeGenerator project to the solution. This project will be an executable tool that generates code prior to compiling the Conductor project.

---
+++ CodeGenerator/src/CodeGenerator.cpp
@@ -0,0 +1,10 @@
+#include <collection/ProgramParameters.h>
+
+int main(const int argc, const char* argv[])
+{
+	const Collection::ProgramParameters params{ argc, argv };
+
+	// TODO generate code
+
+	return 0;
+}
