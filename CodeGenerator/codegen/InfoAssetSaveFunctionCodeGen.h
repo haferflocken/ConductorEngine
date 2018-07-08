29233a85c4bf4af133f886692bad4d7923128ee0
Haferflocken <mooilo.ueu@gmail.com>
2018-07-08 13:21:19 -0700

    Added a comment.

--- CodeGenerator/codegen/InfoAssetSaveFunctionCodeGen.h
+++ CodeGenerator/codegen/InfoAssetSaveFunctionCodeGen.h
@@ -9,6 +9,9 @@ namespace Asset { class RecordSchema; }
 
 namespace CodeGen
 {
+/**
+ * Generates a C++ function which takes an Info Asset's C++ instance and serializes it as JSON.
+ */
 void GenerateInfoInstanceSaveFunction(const Collection::ArrayView<std::string>& namespaceNames,
 	const Asset::RecordSchema& schema, std::ostream& outputStream);
 }
