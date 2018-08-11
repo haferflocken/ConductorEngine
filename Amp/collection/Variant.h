f47cc2b1ebb2ad6d027ca7d1c84168760fbacaf6
haferflocken <mooilo.ueu@gmail.com>
2018-08-12 07:10:04 +1000

    Attempted optimization to Variant::Match. Added some TODOs in Variant.

--- Amp/collection/Variant.h
+++ Amp/collection/Variant.h
@@ -12,9 +12,29 @@ class alignas(Types...) Variant final
 {
 public:
 	Variant()
-		: m_tagBytes{ 0 }
+		: m_tagBytes{ k_invalidTag }
 		, m_data()
 	{}
+
+	template <typename T, size_t Index = Util::IndexOfType<T, Types...>>
+	Variant(const T& v)
+		: m_tagBytes{ static_cast<uint8_t>(Index) }
+	{
+		new (m_data) T(v);
+	}
+
+	template <typename T, size_t Index = Util::IndexOfType<T, Types...>>
+	Variant(T&& v)
+		: m_tagBytes{ static_cast<uint8_t>(Index) }
+	{
+		new (m_data) T(std::move(v));
+	}
+
+	Variant(const Variant& other);
+	Variant& operator=(const Variant& rhs);
+
+	Variant(Variant&& other);
+	Variant& operator=(Variant&& rhs);
 
 	template <size_t Index>
 	Util::TypeAtIndex<Index, Types...>& Get();
@@ -32,6 +52,7 @@ public:
 	void Match(FnTypes&&... functions);
 
 private:
+	static constexpr uint8_t k_invalidTag = UINT8_MAX;
 	static constexpr size_t k_numTagBytes = Util::MaxAlignOf<Types...>();
 	static constexpr size_t k_numDataBytes = Util::MaxAlignedSizeOf<Types...>();
 
@@ -40,6 +61,34 @@ private:
 	uint8_t m_tagBytes[k_numTagBytes];
 	uint8_t m_data[k_numDataBytes];
 };
+
+template <typename... Types>
+Variant<Types...>::Variant(const Variant& other)
+	: m_tagBytes{ other.m_tagBytes[0] }
+{
+	// TODO(variant)
+}
+
+template <typename... Types>
+Variant<Types...>& Variant<Types...>::operator=(const Variant& rhs)
+{
+	// TODO(variant)
+	return *this;
+}
+
+template <typename... Types>
+Variant<Types...>::Variant(Variant&& other)
+	: m_tagBytes{ other.m_tagBytes[0] }
+{
+	// TODO(variant)
+}
+
+template <typename... Types>
+Variant<Types...>& Variant<Types...>::operator=(Variant&& rhs)
+{
+	// TODO(variant)
+	return *this;
+}
 
 template <typename... Types>
 template <size_t Index>
@@ -71,20 +120,30 @@ const Type& Variant<Types...>::Get() const
 	return Get<Util::IndexOfType<Type, Types...>>();
 }
 
+namespace Internal_Variant
+{
+template <typename FnTuple, size_t... Indices>
+void CallIndexInTuple(FnTuple& t, size_t index, std::index_sequence<Indices...>)
+{
+	Util::Or(((index == Indices) ? (std::get<Indices>(t)(), true) : false)...);
+}
+}
+
 template <typename... Types>
 template <typename... FnTypes>
 void Variant<Types...>::Match(FnTypes&&... functions)
 {
-	if (m_tagBytes[0] == 0)
+	static_assert(sizeof...(Types) == sizeof...(FnTypes), "There must be exactly one function per type.");
+	if (m_tagBytes[0] == k_invalidTag)
 	{
 		return;
 	}
 
-	std::function<void()> lambdas[]
-	{
+	auto lambdas = std::make_tuple(
 		[&]() { functions(reinterpret_cast<Types&>(m_data)); }...
-	};
+	);
 
-	lambdas[m_tagBytes[0]]();
+	auto indexSequence = std::index_sequence_for<Types...>();
+	Internal_Variant::CallIndexInTuple(lambdas, m_tagBytes[0], indexSequence);
 }
 }
