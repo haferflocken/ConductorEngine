1cc776905cc97d7a5d40a696ed3301f085a5f82e
haferflocken <mooilo.ueu@gmail.com>
2018-09-09 09:52:08 -0700

    Formatting changes in IClient.cpp

--- Conductor/src/client/IClient.cpp
+++ Conductor/src/client/IClient.cpp
@@ -1,6 +1,9 @@
 #include <client/IClient.h>
 
-void Client::IClient::NotifyOfECSUpdateTransmission(const Collection::Vector<uint8_t>& transmissionBytes)
+namespace Client
+{
+void IClient::NotifyOfECSUpdateTransmission(const Collection::Vector<uint8_t>& transmissionBytes)
 {
 	m_entityManager.ApplyDeltaTransmission(transmissionBytes);
+}
 }
