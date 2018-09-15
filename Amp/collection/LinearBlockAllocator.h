2fcc91d505d2d3acd0dbacfa3f49b33fb59bd495
haferflocken <mooilo.ueu@gmail.com>
2018-09-14 23:38:12 -0700

    Added a basic linear block allocator.

---
+++ Amp/collection/LinearBlockAllocator.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <collection/Vector.h>
+
+#include <cstdint>
+#include <mutex>
+
+namespace Collection
+{
+/**
+ * An allocator that allows fixed size allocations. Implemented as a list of blocks of elements.
+ */
+class LinearBlockAllocator
+{
+public:
+	static constexpr size_t k_numElementsPerBlock = 64;
+
+	template <typename T>
+	static LinearBlockAllocator MakeFor()
+	{
+		return LinearBlockAllocator(alignof(T), sizeof(T));
+	}
+
+	LinearBlockAllocator(size_t alignmentInBytes, size_t sizeInBytes)
+		: m_blocks()
+		, m_mutex()
+		, m_elementAlignmentInBytes(static_cast<uint32_t>(alignmentInBytes))
+		, m_elementSizeInBytes(static_cast<uint32_t>(sizeInBytes))
+	{}
+
+	~LinearBlockAllocator();
+
+	void* Alloc();
+	void Free(void* ptr);
+
+private:
+	struct Block
+	{
+		uint64_t m_vacancyMap{ UINT64_MAX };
+		void* m_memory{ nullptr };
+	};
+	Collection::Vector<Block> m_blocks;
+	std::mutex m_mutex;
+	uint32_t m_elementAlignmentInBytes{ 0 };
+	uint32_t m_elementSizeInBytes{ 0 };
+};
+}
---
+++ Amp/src/collection/LinearBlockAllocator.cpp
@@ -0,0 +1,66 @@
+#include <collection/LinearBlockAllocator.h>
+
+#include <dev/Dev.h>
+
+namespace Collection
+{
+LinearBlockAllocator::~LinearBlockAllocator()
+{
+	Dev::Assert(m_blocks.IsEmpty(),
+		"LinearAllocators should not be destroyed until everything allocated from them is freed.");
+}
+
+void* LinearBlockAllocator::Alloc()
+{
+	std::lock_guard guard{ m_mutex };
+
+	if ((!m_blocks.IsEmpty()) && m_blocks.Back().m_vacancyMap != 0)
+	{
+		Block& block = m_blocks.Back();
+
+		unsigned long index;
+		_BitScanReverse64(&index, block.m_vacancyMap);
+
+		uint8_t* result = reinterpret_cast<uint8_t*>(block.m_memory) + (index * m_elementSizeInBytes);
+		block.m_vacancyMap ^= (1ui64 << index);
+		return result;
+	}
+
+	Block& block = m_blocks.Emplace();
+	block.m_vacancyMap ^= 1;
+	block.m_memory = _aligned_malloc(m_elementAlignmentInBytes, m_elementSizeInBytes * k_numElementsPerBlock);
+
+	return block.m_memory;
+}
+
+void LinearBlockAllocator::Free(void* ptr)
+{
+	std::lock_guard guard{ m_mutex };
+
+	for (size_t i = 0, iEnd = m_blocks.Size(); i < iEnd; ++i)
+	{
+		Block& block = m_blocks[i];
+		const uint8_t* const blockMemoryBegin = reinterpret_cast<const uint8_t*>(block.m_memory);
+		const uint8_t* const blockMemoryEnd = blockMemoryBegin + (m_elementSizeInBytes * k_numElementsPerBlock);
+
+		if (ptr >= block.m_memory && ptr < blockMemoryEnd)
+		{
+			const std::ptrdiff_t indexInBlock =
+				(reinterpret_cast<const uint8_t*>(ptr) - blockMemoryBegin) / m_elementSizeInBytes;
+
+			Dev::FatalAssert((block.m_vacancyMap & (1ui64 << indexInBlock)) == 0,
+				"Cannot free an already freed pointer in LinearAllocator.");
+
+			block.m_vacancyMap |= (1ui64 << indexInBlock);
+
+			if (block.m_vacancyMap == UINT64_MAX)
+			{
+				_aligned_free(block.m_memory);
+				m_blocks.SwapWithAndRemoveLast(i);
+			}
+			return;
+		}
+	}
+	Dev::FatalError("LinearAllocator failed to free pointer %p.", ptr);
+}
+}
