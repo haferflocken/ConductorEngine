73e3dcbbce619a4fad6c6ba985f658758ad7a03f
haferflocken <mooilo.ueu@gmail.com>
2018-12-17 23:00:03 -0800

    Cleaned up forward declares in SceneTransformComponentInfo.h.

--- Conductor/scene/SceneTransformComponentInfo.h
+++ Conductor/scene/SceneTransformComponentInfo.h
@@ -2,10 +2,6 @@
 
 #include <ecs/ComponentInfo.h>
 
-namespace Behave { class BehaviourTreeManager; }
-namespace JSON { class JSONObject; }
-namespace Mem { template <typename T> class UniquePtr; }
-
 namespace Scene
 {
 class SceneTransformComponentInfo final : public ECS::ComponentInfo
