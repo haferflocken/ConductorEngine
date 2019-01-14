449ffe8595a3e2b727980e86d06623b38d4f4d02
haferflocken <mooilo.ueu@gmail.com>
2019-01-13 21:58:55 -0800

    Added various member functions and operators to Vector4.

--- Conductor/math/Vector4.h
+++ Conductor/math/Vector4.h
@@ -16,6 +16,19 @@ public:
 	Vector4(const Vector4&) = default;
 	Vector4& operator=(const Vector4&) = default;
 
+	float LengthSquared() const;
+	float Length() const;
+
+	void operator+=(const Vector4& rhs);
+	void operator-=(const Vector4& rhs);
+	void operator*=(float rhs);
+	void operator/=(float rhs);
+
+	Vector4 operator+(const Vector4& rhs) const;
+	Vector4 operator-(const Vector4& rhs) const;
+	Vector4 operator*(float rhs) const;
+	Vector4 operator/(float rhs) const;
+
 	float Dot(const Vector4& rhs) const;
 
 	float x;
@@ -38,6 +51,67 @@ inline Vector4::Vector4(float _x, float _y, float _z, float _w)
 	, w(_w)
 {}
 
+inline float Vector4::LengthSquared() const
+{
+	return (x * x) + (y * y) + (z * z) + (w * w);
+}
+
+inline float Vector4::Length() const
+{
+	return sqrtf(LengthSquared());
+}
+
+inline void Vector4::operator+=(const Vector4& rhs)
+{
+	x += rhs.x;
+	y += rhs.y;
+	z += rhs.z;
+	w += rhs.w;
+}
+
+inline void Vector4::operator-=(const Vector4& rhs)
+{
+	x -= rhs.x;
+	y -= rhs.y;
+	z -= rhs.z;
+	w -= rhs.w;
+}
+
+inline void Vector4::operator*=(float rhs)
+{
+	x *= rhs;
+	y *= rhs;
+	z *= rhs;
+	w *= rhs;
+}
+
+inline void Vector4::operator/=(float rhs)
+{
+	x /= rhs;
+	y /= rhs;
+	z /= rhs;
+	w /= rhs;
+}
+
+inline Vector4 Vector4::operator+(const Vector4& rhs) const
+{
+	return Vector4(x + rhs.x, y + rhs.y, z + rhs.z, w + rhs.w);
+}
+
+inline Vector4 Vector4::operator-(const Vector4& rhs) const
+{
+	return Vector4(x - rhs.x, y - rhs.y, z - rhs.z, w - rhs.w);
+}
+
+inline Vector4 Vector4::operator*(float rhs) const
+{
+	return Vector4(x * rhs, y * rhs, z * rhs, w * rhs);
+}
+
+inline Vector4 Vector4::operator/(float rhs) const
+{
+	return Vector4(x / rhs, y / rhs, z / rhs, w / rhs);
+}
 inline float Vector4::Dot(const Vector4& rhs) const
 {
 	const float result = (x * rhs.x) + (y * rhs.y) + (z * rhs.z) + (w * rhs.w);
