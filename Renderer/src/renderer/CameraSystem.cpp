cb2c80b297a1ae1a1e4c8535441e825262fd23b0
haferflocken <mooilo.ueu@gmail.com>
2019-01-18 09:01:52 -0800

    Guaranteed default camera if one doesn't exist

--- Renderer/src/renderer/CameraSystem.cpp
+++ Renderer/src/renderer/CameraSystem.cpp
@@ -10,6 +10,20 @@ void CameraSystem::Update(const Unit::Time::Millisecond delta,
 	Collection::Vector<std::function<void(ECS::EntityManager&)>>& deferredFunctions) const
 {
 	const bool homogeneousDepth = bgfx::getCaps()->homogeneousDepth;
+
+	// Construct and apply a default camera for the scene view.
+	{
+		float viewMatrix[16];
+		bx::mtxIdentity(viewMatrix);
+
+		float projectionMatrix[16];
+		bx::mtxProj(projectionMatrix, 60.0f, m_aspectRatio, 0.1f, 100.0f, homogeneousDepth);
+
+		bgfx::setViewTransform(k_sceneViewID, viewMatrix, projectionMatrix);
+		bgfx::setViewRect(k_sceneViewID, 0, 0, m_widthPixels, m_heightPixels);
+	}
+
+	// Loop over all cameras in the scene and apply them.
 	for (const auto& ecsGroup : ecsGroups)
 	{
 		const auto& transformComponent = ecsGroup.Get<const Scene::SceneTransformComponent>();
