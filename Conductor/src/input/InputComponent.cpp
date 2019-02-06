914f9e6b75869a4ff7c66bc44f25ac66084493e3
haferflocken <mooilo.ueu@gmail.com>
2019-02-06 08:55:20 -0800

    Serialization/deserialization for InputComponent.

--- Conductor/src/input/InputComponent.cpp
+++ Conductor/src/input/InputComponent.cpp
@@ -1,7 +1,9 @@
 #include <input/InputComponent.h>
 
 #include <ecs/ComponentVector.h>
+#include <mem/DeserializeLittleEndian.h>
 #include <mem/InspectorInfo.h>
+#include <mem/SerializeLittleEndian.h>
 
 namespace Input
 {
@@ -10,16 +12,40 @@ const Mem::InspectorInfoTypeHash InputComponent::k_inspectorInfoTypeHash = MakeI
 
 void InputComponent::FullySerialize(const InputComponent& component, Collection::Vector<uint8_t>& outBytes)
 {
-	// TODO(info) fully serialize
+	// Only the client ID and desired inputs are saved.
+	Mem::LittleEndian::Serialize(component.m_clientID.GetN(), outBytes);
+
+	Mem::LittleEndian::Serialize(component.m_inputMap.Size(), outBytes);
+	for (const auto& entry : component.m_inputMap)
+	{
+		const char* const inputName = Util::ReverseHash(entry.first);
+		Mem::LittleEndian::Serialize(inputName, outBytes);
+	}
 }
 
 void InputComponent::ApplyFullSerialization(
 	Asset::AssetManager& assetManager, InputComponent& component, const uint8_t*& bytes, const uint8_t* bytesEnd)
 {
-	// TODO(info) what inputs does this component care about?
-	/*for (const auto& nameHash : componentInfo.m_inputNameHashes)
+	const auto maybeClientID = Mem::LittleEndian::DeserializeUi16(bytes, bytesEnd);
+	if (!maybeClientID.second)
 	{
-		inputComponent.m_inputMap[nameHash];
-	}*/
+		return;
+	}
+
+	const auto maybeNumInputs = Mem::LittleEndian::DeserializeUi32(bytes, bytesEnd);
+	if (!maybeNumInputs.second)
+	{
+		return;
+	}
+	for (size_t i = 0; i < maybeNumInputs.first; ++i)
+	{
+		char inputNameBuffer[64];
+		if (!Mem::LittleEndian::DeserializeString(bytes, bytesEnd, inputNameBuffer))
+		{
+			return;
+		}
+		const Util::StringHash inputNameHash = Util::CalcHash(inputNameBuffer);
+		component.m_inputMap[inputNameHash];
+	}
 }
 }
