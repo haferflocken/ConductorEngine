ac7934649bd898fdb53555a1a8e7a43fde678c02
haferflocken <mooilo.ueu@gmail.com>
2019-02-17 15:19:24 -0800

    Serialize and deserialize SkeletonRootComponent.

--- Conductor/src/mesh/SkeletonRootComponent.cpp
+++ Conductor/src/mesh/SkeletonRootComponent.cpp
@@ -1,6 +1,9 @@
 #include <mesh/SkeletonRootComponent.h>
 
+#include <mem/DeserializeLittleEndian.h>
 #include <mem/InspectorInfo.h>
+#include <mem/SerializeLittleEndian.h>
+#include <scene/SceneTransformComponent.h>
 
 namespace Mesh
 {
@@ -10,12 +13,36 @@ const Mem::InspectorInfoTypeHash SkeletonRootComponent::k_inspectorInfoTypeHash 
 void SkeletonRootComponent::FullySerialize(
 	const SkeletonRootComponent& component, Collection::Vector<uint8_t>& outBytes)
 {
-	// TODO(mesh) serialize the SkeletonRootComponent
+	const uint16_t numBones = static_cast<uint16_t>(component.m_boneTransformComponentIDs.Size());
+	Mem::LittleEndian::Serialize(numBones, outBytes);
+
+	for (const auto& componentID : component.m_boneTransformComponentIDs)
+	{
+		Mem::LittleEndian::Serialize(uint64_t(componentID.GetUniqueID()), outBytes);
+	}
 }
 
 void SkeletonRootComponent::ApplyFullSerialization(
 	Asset::AssetManager& assetManager, SkeletonRootComponent& component, const uint8_t*& bytes, const uint8_t* bytesEnd)
 {
-	// TODO(mesh) deserialize the SkeletonRootComponent
+	const auto maybeNumBones = Mem::LittleEndian::DeserializeUi16(bytes, bytesEnd);
+	if (!maybeNumBones.second)
+	{
+		return;
+	}
+	const uint32_t numBones = maybeNumBones.first;
+	component.m_boneTransformComponentIDs.Resize(numBones);
+	for (size_t i = 0; i < numBones; ++i)
+	{
+		const auto maybeComponentUniqueID = Mem::LittleEndian::DeserializeUi64(bytes, bytesEnd);
+		if (!maybeComponentUniqueID.second)
+		{
+			return;
+		}
+
+		const uint64_t componentUniqueID = maybeComponentUniqueID.first;
+		component.m_boneTransformComponentIDs[i] =
+			ECS::ComponentID(Scene::SceneTransformComponent::k_type, componentUniqueID);
+	}
 }
 }
