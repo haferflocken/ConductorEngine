7ab179162257eade8a2af37dacc2e2002a3b1d13
haferflocken <mooilo.ueu@gmail.com>
2019-05-23 00:09:41 -0700

    Added a mechanism to access profiling data.

--- Amp/dev/Profiler.h
+++ Amp/dev/Profiler.h
@@ -1,33 +1,34 @@
 #pragma once
 
+#include <collection/Vector.h>
+#include <mem/UniquePtr.h>
+
+#include <array>
 #include <chrono>
 #include <cstdint>
+#include <functional>
 
 #define AMP_PROFILING_ENABLED 1
 
 #if AMP_PROFILING_ENABLED == 1
-#define AMP_PROFILE_SCOPE() ::Dev::ScopedFrame profilerFrame{ __func__ }
-#define AMP_INIT_PROFILER() ::Dev::ScopedProfilerThread scopedProfilerThread
+#define AMP_PROFILE_SCOPE() ::Dev::Profiler::ScopedFrame profilerFrame{ __func__ }
+#define AMP_INIT_PROFILER() ::Dev::Profiler::ScopedProfilerThread scopedProfilerThread
 #else
 #define AMP_PROFILE_SCOPE()
 #define AMP_INIT_PROFILER()
 #endif
 
-namespace Dev
+namespace Dev::Profiler
 {
-struct FrameRecord final
-{
-	uint64_t m_parentFrameID;
-	uint64_t m_beginPoint;
-	uint64_t m_durationNanoseconds;
-	uint64_t m_depth;
-	const char* m_frameName;
-};
+class FrameRecordMap;
 
 // Push a new frame and return the ID of the previous one.
 uint64_t PushFrame(const char* const name, const uint64_t beginNanoseconds);
 // Pop the current frame and restore the previous one.
 void PopFrame(const uint64_t endNanoseconds, const uint64_t previousFrameID);
+// Iterate over the frame record maps. fn is called with (profiler thread ID, frame record map).
+// This is the only mechanism to access profiling data.
+void ForEachFrameRecordMap(const std::function<void(uint64_t, const FrameRecordMap&)>& fn);
 
 /**
  * Pushes a frame on construction; pops a frame on destruction.
@@ -50,6 +51,47 @@ public:
 		const std::chrono::nanoseconds time = std::chrono::steady_clock::now().time_since_epoch();
 		PopFrame(time.count(), m_previousFrameID);
 	}
+};
+
+/**
+ * FrameRecordMap is used to process the profiler event queues for a thread and stores the result.
+ */
+class FrameRecordMap final
+{
+public:
+	struct FrameRecord final
+	{
+		uint64_t m_parentFrameID;
+		uint64_t m_beginPoint;
+		uint64_t m_durationNanoseconds;
+		uint64_t m_depth;
+		const char* m_frameName;
+	};
+
+public:
+	FrameRecordMap();
+
+	FrameRecord& GetFrameRecord(uint64_t frameID);
+	const FrameRecord& GetFrameRecord(uint64_t frameID) const;
+
+	uint32_t GetNumFrameRecords() const;
+
+	void PushFrame(uint64_t frameID, uint64_t beginPoint, const char* name);
+	void PopFrame(uint64_t frameID, uint64_t endPoint);
+
+private:
+	static constexpr size_t k_frameRecordBlockSize = 4096;
+	using FrameRecordBlock = std::array<FrameRecord, k_frameRecordBlockSize>;
+
+	FrameRecord& CreateFrameRecord();
+
+private:
+	uint64_t m_currentFrameID;
+	uint64_t m_currentDepth;
+	// Because frame IDs increment upwards from 0, we can use them as indices.
+	// We allocate frame records in blocks of 4096 that don't move around to avoid copying them.
+	uint32_t m_numRecordsInLastBlock;
+	Collection::Vector<Mem::UniquePtr<FrameRecordBlock>> m_frameRecordBlocks;
 };
 
 /**
--- Amp/src/dev/Profiler.cpp
+++ Amp/src/dev/Profiler.cpp
@@ -2,9 +2,6 @@
 
 #include <collection/LinearBlockHashMap.h>
 #include <collection/LocklessQueue.h>
-#include <mem/UniquePtr.h>
-
-#include <array>
 
 namespace Internal_Profiler
 {
@@ -61,101 +58,9 @@ struct ThreadLocalProfiler final
 	Collection::LocklessQueue<ProfilerEvent> m_eventQueue{ 0xFFFF };
 };
 
-/**
- * FrameRecordMap is used to process the profiler event queues for a thread and stores the result.
- */
-class FrameRecordMap final
-{
-public:
-	FrameRecordMap()
-		: m_currentFrameID(0)
-		, m_currentDepth(1)
-		, m_numRecordsInLastBlock(1)
-		, m_frameRecordBlocks(32)
-	{
-		m_frameRecordBlocks.Add(Mem::MakeUnique<FrameRecordBlock>());
-		FrameRecordBlock& block = *m_frameRecordBlocks.Back();
-		Dev::FrameRecord& rootRecord = block.front();
-		rootRecord.m_parentFrameID = 0;
-		rootRecord.m_beginPoint = 0;
-		rootRecord.m_durationNanoseconds = UINT64_MAX;
-		rootRecord.m_depth = 0;
-		rootRecord.m_frameName = "FrameRecordMap Root Frame";
-	}
-
-	void PushFrame(uint64_t frameID, uint64_t beginPoint, const char* name)
-	{
-		AMP_ASSERT(frameID == GetNumFrameRecords(), "");
-
-		const uint64_t parentFrameID = m_currentFrameID;
-		m_currentFrameID = frameID;
-
-		Dev::FrameRecord& frameRecord = CreateFrameRecord();
-		frameRecord.m_parentFrameID = parentFrameID;
-		frameRecord.m_beginPoint = beginPoint;
-		frameRecord.m_durationNanoseconds = UINT64_MAX;
-		frameRecord.m_depth = m_currentDepth;
-		frameRecord.m_frameName = name;
-
-		++m_currentDepth;
-	}
-
-	void PopFrame(uint64_t frameID, uint64_t endPoint)
-	{
-		AMP_ASSERT(frameID == m_currentFrameID, "");
-		Dev::FrameRecord& frameRecord = GetFrameRecord(frameID);
-		frameRecord.m_durationNanoseconds = endPoint - frameRecord.m_beginPoint;
-
-		m_currentFrameID = frameRecord.m_parentFrameID;
-		--m_currentDepth;
-	}
-
-	Dev::FrameRecord& GetFrameRecord(uint64_t frameID)
-	{
-		const size_t blockIndex = frameID / k_frameRecordBlockSize;
-		const size_t indexInBlock = frameID % k_frameRecordBlockSize;
-		FrameRecordBlock& block = *m_frameRecordBlocks[blockIndex];
-		return block[indexInBlock];
-	}
-
-	uint32_t GetNumFrameRecords() const
-	{
-		return (m_frameRecordBlocks.Size() - 1) * k_frameRecordBlockSize + m_numRecordsInLastBlock;
-	}
-
-private:
-	static constexpr size_t k_frameRecordBlockSize = 4096;
-	using FrameRecordBlock = std::array<Dev::FrameRecord, k_frameRecordBlockSize>;
-
-	Dev::FrameRecord& CreateFrameRecord()
-	{
-		if (m_numRecordsInLastBlock < k_frameRecordBlockSize)
-		{
-			const uint32_t indexInBlock = m_numRecordsInLastBlock;
-			++m_numRecordsInLastBlock;
-			FrameRecordBlock& block = *m_frameRecordBlocks.Back();
-			return block[indexInBlock];
-		}
-
-		m_numRecordsInLastBlock = 1;
-		m_frameRecordBlocks.Add(Mem::MakeUnique<FrameRecordBlock>());
-		FrameRecordBlock& block = *m_frameRecordBlocks.Back();
-		return block.front();
-	}
-
-private:
-	uint64_t m_currentFrameID;
-	uint64_t m_currentDepth;
-	// Because frame IDs increment upwards from 0, we can use them as indices.
-	// We allocate frame records in blocks of 4096 that don't move around to avoid copying them.
-	uint32_t m_numRecordsInLastBlock;
-	Collection::Vector<Mem::UniquePtr<FrameRecordBlock>> m_frameRecordBlocks; 
-};
-
-
 thread_local ThreadLocalProfiler* tls_profiler = nullptr;
 Collection::LinearBlockHashMap<uint64_t, ThreadLocalProfiler, Collection::I64HashFunctor> g_profilerRegistry{ Collection::I64HashFunctor(), 4 };
-Collection::LinearBlockHashMap<uint64_t, FrameRecordMap, Collection::I64HashFunctor> g_frameRecordsByRegistryID{ Collection::I64HashFunctor(), 4 };
+Collection::LinearBlockHashMap<uint64_t, Dev::Profiler::FrameRecordMap, Collection::I64HashFunctor> g_frameRecordsByRegistryID{ Collection::I64HashFunctor(), 4 };
 uint64_t g_nextProfilerRegistryID = 0;
 std::mutex g_profilerRegistryMutex;
 
@@ -171,7 +76,7 @@ void ProcessProfilerEventQueues()
 	{
 		const uint64_t profiledThreadID = entry.key;
 		ThreadLocalProfiler& profiler = *entry.value;
-		FrameRecordMap& frameRecordMap = *g_frameRecordsByRegistryID.Find(profiledThreadID);
+		Dev::Profiler::FrameRecordMap& frameRecordMap = *g_frameRecordsByRegistryID.Find(profiledThreadID);
 
 		ProfilerEvent event = ProfilerEvent::MakeNullEvent();
 		while (profiler.m_eventQueue.TryPop(event))
@@ -202,7 +107,7 @@ void ProfilerThreadFunction()
 }
 }
 
-uint64_t Dev::PushFrame(const char* const name, const uint64_t beginNanoseconds)
+uint64_t Dev::Profiler::PushFrame(const char* const name, const uint64_t beginNanoseconds)
 {
 	using namespace Internal_Profiler;
 
@@ -228,7 +133,7 @@ uint64_t Dev::PushFrame(const char* const name, const uint64_t beginNanoseconds)
 	return previousFrameID;
 }
 
-void Dev::PopFrame(const uint64_t endNanoseconds, const uint64_t previousFrameID)
+void Dev::Profiler::PopFrame(const uint64_t endNanoseconds, const uint64_t previousFrameID)
 {
 	using namespace Internal_Profiler;
 
@@ -238,16 +143,107 @@ void Dev::PopFrame(const uint64_t endNanoseconds, const uint64_t previousFrameID
 	profiler.m_currentFrameID = previousFrameID;
 }
 
-Dev::ScopedProfilerThread::ScopedProfilerThread()
+void Dev::Profiler::ForEachFrameRecordMap(const std::function<void(uint64_t, const FrameRecordMap&)>& fn)
+{
+	using namespace Internal_Profiler;
+	std::unique_lock<std::mutex> lock{ g_profilerRegistryMutex };
+
+	for (const auto& entry : g_frameRecordsByRegistryID.GetKeyValueView())
+	{
+		fn(entry.key, *entry.value);
+	}
+}
+
+namespace Dev::Profiler
+{
+FrameRecordMap::FrameRecordMap()
+	: m_currentFrameID(0)
+	, m_currentDepth(1)
+	, m_numRecordsInLastBlock(1)
+	, m_frameRecordBlocks(32)
+{
+	m_frameRecordBlocks.Add(Mem::MakeUnique<FrameRecordBlock>());
+	FrameRecordBlock& block = *m_frameRecordBlocks.Back();
+	FrameRecord& rootRecord = block.front();
+	rootRecord.m_parentFrameID = 0;
+	rootRecord.m_beginPoint = 0;
+	rootRecord.m_durationNanoseconds = UINT64_MAX;
+	rootRecord.m_depth = 0;
+	rootRecord.m_frameName = "FrameRecordMap Root Frame";
+}
+
+FrameRecordMap::FrameRecord& FrameRecordMap::GetFrameRecord(uint64_t frameID)
+{
+	return const_cast<FrameRecord&>(static_cast<const FrameRecordMap*>(this)->GetFrameRecord(frameID));
+}
+
+const FrameRecordMap::FrameRecord& FrameRecordMap::GetFrameRecord(uint64_t frameID) const
+{
+	const size_t blockIndex = frameID / k_frameRecordBlockSize;
+	const size_t indexInBlock = frameID % k_frameRecordBlockSize;
+	const FrameRecordBlock& block = *m_frameRecordBlocks[blockIndex];
+	return block[indexInBlock];
+}
+
+uint32_t FrameRecordMap::GetNumFrameRecords() const
+{
+	return (m_frameRecordBlocks.Size() - 1) * k_frameRecordBlockSize + m_numRecordsInLastBlock;
+}
+
+void FrameRecordMap::PushFrame(uint64_t frameID, uint64_t beginPoint, const char* name)
+{
+	AMP_ASSERT(frameID == GetNumFrameRecords(), "");
+
+	const uint64_t parentFrameID = m_currentFrameID;
+	m_currentFrameID = frameID;
+
+	FrameRecord& frameRecord = CreateFrameRecord();
+	frameRecord.m_parentFrameID = parentFrameID;
+	frameRecord.m_beginPoint = beginPoint;
+	frameRecord.m_durationNanoseconds = UINT64_MAX;
+	frameRecord.m_depth = m_currentDepth;
+	frameRecord.m_frameName = name;
+
+	++m_currentDepth;
+}
+
+void FrameRecordMap::PopFrame(uint64_t frameID, uint64_t endPoint)
+{
+	AMP_ASSERT(frameID == m_currentFrameID, "");
+	FrameRecord& frameRecord = GetFrameRecord(frameID);
+	frameRecord.m_durationNanoseconds = endPoint - frameRecord.m_beginPoint;
+
+	m_currentFrameID = frameRecord.m_parentFrameID;
+	--m_currentDepth;
+}
+
+FrameRecordMap::FrameRecord& FrameRecordMap::CreateFrameRecord()
+{
+	if (m_numRecordsInLastBlock < k_frameRecordBlockSize)
+	{
+		const uint32_t indexInBlock = m_numRecordsInLastBlock;
+		++m_numRecordsInLastBlock;
+		FrameRecordBlock& block = *m_frameRecordBlocks.Back();
+		return block[indexInBlock];
+	}
+
+	m_numRecordsInLastBlock = 1;
+	m_frameRecordBlocks.Add(Mem::MakeUnique<FrameRecordBlock>());
+	FrameRecordBlock& block = *m_frameRecordBlocks.Back();
+	return block.front();
+}
+
+ScopedProfilerThread::ScopedProfilerThread()
 {
 	using namespace Internal_Profiler;
 	g_shouldRunProfilerThread = true;
 	g_profilerThread = std::thread(&ProfilerThreadFunction);
 }
 
-Dev::ScopedProfilerThread::~ScopedProfilerThread()
+ScopedProfilerThread::~ScopedProfilerThread()
 {
 	using namespace Internal_Profiler;
 	g_shouldRunProfilerThread = false;
 	g_profilerThread.join();
+}
 }
